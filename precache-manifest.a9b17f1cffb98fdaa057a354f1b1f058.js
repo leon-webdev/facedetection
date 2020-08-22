5ff7b519b39c05ddb1f8c483c1b65492df30a1ae
Leon G. Martinez Ostos <leonma19@gmail.com>
2020-08-21 19:49:20 -0500

    Updates

---
+++ precache-manifest.a9b17f1cffb98fdaa057a354f1b1f058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95050496103d01a8f4b36008fb61b5ac",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "a680c4f4ff8ba09270f8",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e37bb8e9aaf68b9f83a1",
+    "url": "/facedetection/static/css/main.03512010.chunk.css"
+  },
+  {
+    "revision": "a680c4f4ff8ba09270f8",
+    "url": "/facedetection/static/js/2.aafe8f77.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.aafe8f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37bb8e9aaf68b9f83a1",
+    "url": "/facedetection/static/js/main.d840de49.chunk.js"
+  },
+  {
+    "revision": "99d29fa1a4ca556ebf26",
+    "url": "/facedetection/static/js/runtime-main.7cf075ce.js"
+  }
+]);
